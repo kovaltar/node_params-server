edd791566700ad4eb99908758bf68940c83a1a06
kovaltar <kovaltar@gmail.com>
2025-04-15 23:46:21 +0200

    Solution for node_params-server

--- src/createServer.js
+++ src/createServer.js
@@ -1,9 +1,32 @@
 /* eslint-disable no-console */
 'use strict';
 
+const http = require('node:http');
+
 function createServer() {
-  /* Write your code here */
-  // Return instance of http.Server class
+  const server = http.createServer((req, res) => {
+    let result = {};
+    let reqUrl;
+
+    try {
+      reqUrl = new URL(req.url, 'http://localhost:5701');
+    } catch (err) {
+      console.error({ message: 'Invalid request URL' });
+    }
+
+    const parts = reqUrl.pathname.split('/').filter((item) => item !== '');
+    const query = Object.fromEntries(reqUrl.searchParams.entries());
+
+    result = { parts, query };
+
+    console.log(JSON.stringify(result));
+
+    res.statusCode = 200;
+    res.setHeader('Content-Type', 'application/json');
+    res.end(JSON.stringify(result));
+  });
+
+  return server;
 }
 
 module.exports = {
